07865406e3065e82f64d2b1b581af0c4a473e6bc
SeHyoung1024 <ddd376@naver.com>
2024-09-19 17:48:50 +0900

    FirstCommit

---
+++ src/main/java/com/spring_boot/projectEx/SpringBootProjectFinalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot.projectEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectFinalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectFinalprojectApplication.class, args);
+	}
+
+}
